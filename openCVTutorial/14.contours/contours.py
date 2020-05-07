f7caa699e852c8d7ba470c1e829aaee1a095d91d
xiashiwenao <xiashiwendao@sina.com>
2020-05-07 11:54:35 +0800

    update

--- openCVTutorial/14.contours/contours.py
+++ openCVTutorial/14.contours/contours.py
@@ -5,7 +5,8 @@ def show_img(img, window_name='window_img'):
     cv2.imshow(window_name, img)
     cv2.waitKey(0)
 img = cv2.imread('handwrite.jpg')
-show_img(img, 'handwrite_show')
-_, thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+# show_img(img, 'handwrite_show')
+_, thresh = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY_INV)
+
 show_img(thresh, 'thresh')
 # contours, hierarchy = cv2.findContours(thresh, 3, 2)
